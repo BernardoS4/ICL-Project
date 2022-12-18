663cc81520feef2e208bd41bc9496707d76ee8be
Alexandre Oliveira <91698340+nedzer0@users.noreply.github.com>
2022-12-18 23:26:21 +0000

    Update ASTPrint.java

--- project/src/ast/ASTPrint.java
+++ project/src/ast/ASTPrint.java
@@ -17,6 +17,10 @@ public class ASTPrint implements ASTNode {
     @Override
     public void compile(CodeBlock code, Environment<Coordinate> e) {
         val.compile(code, e);
-        code.emit("");
+        code.emit("sipush" + val);
+        code.emit("istore_1");
+        code.emit("getstatic java/lang/System.out Ljava/io/PrintStream");
+        code.emit("iload_1");
+        code.emit("invokespecial java/io/PrintStream.println (I)V");
     }
 }
