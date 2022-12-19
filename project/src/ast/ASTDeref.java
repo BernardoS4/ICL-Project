f9fd737190a9b26ecf85295d148c5af6457a1bee
BernardoS4 <bsa.silva@campus.fct.unl.pt>
2022-12-19 11:50:00 +0000

    interpretador feito

--- project/src/ast/ASTDeref.java
+++ project/src/ast/ASTDeref.java
@@ -10,18 +10,13 @@ public class ASTDeref implements ASTNode {
 
     public IValue eval(Environment<IValue> e) {
         IValue v1 = val.eval(e);
-        return v1;
 
-        /*
-         * if (v1 instanceof VBool)
-         * return new VBool(((VBool) v1).getVal());
-         * else if (v1 instanceof VInt)
-         * return new VInt(((VInt) v1).getVal());
-         * else if (v1 instanceof VCell)
-         * return new VCell(((VCell) v1).getVal());
-         * 
-         * throw new RuntimeException("illegal arguments to ! operator");
-         */
+        if (v1 instanceof VCell) {
+            return ((VCell) v1).getVal();
+        }
+
+        throw new RuntimeException("illegal arguments to ! operator");
+
     }
 
     @Override
--- project/src/ast/ASTWhile.java
+++ project/src/ast/ASTWhile.java
@@ -13,13 +13,13 @@ public class ASTWhile implements ASTNode {
         IValue v1 = cond.eval(e);
 
         if (v1 instanceof VBool) {
-            IValue v2 = null;
+            IValue v2;
             while (((VBool) v1).getVal()) {
                 v2 = exp.eval(e);
                 System.out.println(v2.show());
                 v1 = cond.eval(e);
             }
-            return v2;
+            return v1;
         }
         throw new RuntimeException("illegal arguments to while operation");
     }
